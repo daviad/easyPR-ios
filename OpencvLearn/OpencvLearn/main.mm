e7e42945ecb7ba2f941f281209c169afb45c69d4
dxw <xiuwei.ding@loocha.com>
2018-08-23 18:04:12 +0800

    add

--- OpencvLearn/OpencvLearn/main.mm
+++ OpencvLearn/OpencvLearn/main.mm
@@ -6,15 +6,17 @@
 //  Copyright © 2018年  dingxiuwei. All rights reserved.
 //
 
-#include<opencv2\imgproc\imgproc.hpp>
-#include<opencv2\opencv.hpp>
-#include<opencv2\highgui\highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+#import <Foundation/Foundation.h>
+
 using namespace std;
 using namespace cv;
 
 int main()
 {
-    Mat Image = imread(".//res//street.jpg", 0);
+    NSString *resourcePath=[[NSBundle mainBundle] resourcePath];
+    Mat Image = imread("/Users/dxw/Desktop/github/MLDemo/easyPR-ios/EasyPR/EasyPR/images/wATH859.jpg", 0);
     Mat CannyImg;
     Canny(Image, CannyImg, 140, 250, 3);
     imshow("CannyImg", CannyImg);
@@ -26,10 +28,10 @@ int main()
     HoughLinesP(CannyImg, Lines, 1, CV_PI / 360, 170,30,15);
     for (size_t i = 0; i < Lines.size(); i++)
     {
-        line(DstImg, Point(Lines[i][0], Lines[i][1]), Point(Lines[i][2], Lines[i][3]), Scalar(0, 0, 255), 2, 8);
+        line(DstImg, cv::Point(Lines[i][0], Lines[i][1]), cv::Point(Lines[i][2], Lines[i][3]), cv::Scalar(0, 0, 255), 2, 8);
     }
     imshow("HoughLines_Detect", DstImg);
-    imwrite(".//res//HoughLines_Detect.jpg", DstImg);
+    imwrite("./res/HoughLines_Detect.jpg", DstImg);
     waitKey(0);
     return 0;
 }
